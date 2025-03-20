2636f5b1159d76fbceec062db90405adcce97bac
Sebastian Villahermosa <typhoon12vm@gmail.com>
2025-03-20 17:39:21 -0300

    Create buildAssets-script

---
+++ Sources/buildAssets-script.py
@@ -0,0 +1,34 @@
+import os
+import json
+
+def create_xcassets(icons_dir, xcassets_dir):
+    appiconset_dir = os.path.join(xcassets_dir, "AppIcon.appiconset")
+    os.makedirs(appiconset_dir, exist_ok=True)
+
+    images = []
+    for icon in os.listdir(icons_dir):
+        if icon.endswith((".png", ".jpg", ".jpeg")):
+            filename = os.path.basename(icon)
+            images.append({
+                "filename": filename,
+                "idiom": "universal",
+                "scale": "1x" if "1x" in filename else "2x" if "2x" in filename else "3x",
+            })
+            # Copy the icon file to the appiconset directory
+            os.system(f"cp {os.path.join(icons_dir, icon)} {appiconset_dir}")
+
+    contents = {
+        "images": images,
+        "info": {
+            "version": 1,
+            "author": "xcode"
+        }
+    }
+
+    with open(os.path.join(appiconset_dir, "Contents.json"), "w") as f:
+        json.dump(contents, f, indent=2)
+
+if __name__ == "__main__":
+    icons_dir = "icons"
+    xcassets_dir = "MyIcons.xcassets"
+    create_xcassets(icons_dir, xcassets_dir)
