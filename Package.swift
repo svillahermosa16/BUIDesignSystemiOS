31f4a73487bac9387c8c1f31b4b2cf15a204a5ad
Sebastian Villahermosa <typhoon12vm@gmail.com>
2025-03-21 13:32:09 -0300

    Added mediahelper, changed Assets folder

--- Package.swift
+++ Package.swift
@@ -15,17 +15,12 @@ let package = Package(
             // Targets are the basic building blocks of a package, defining a module or a test suite.
             // Targets can depend on other targets in this package and products from dependencies.
             .target(
-                name: "BUIDesignSystemiOS",
-                resources: [
-                    .process("Resources/Media.xcassets")
-                ]),
+                name: "BUIDesignSystemiOS"
+                ),
                 
             .testTarget(
                 name: "BUIDesignSystemiOSTests",
-                dependencies: ["BUIDesignSystemiOS"],
-                resources: [
-                    .process("Resources/Media.xcassets")
-                ]
+                dependencies: ["BUIDesignSystemiOS"]
             ),
         ]
 )
---
+++ Sources/MediaHelper.swift
@@ -0,0 +1,13 @@
+//
+//  MediaHelper.swift
+//  BUIDesignSystemiOS
+//
+//  Created by VILLAHERMOSA SEBASTIAN on 21/03/2025.
+//
+
+import UIKit
+public struct MediaHelper {
+    public static func imageFromPackage(named imageName: String) -> UIImage? {
+        return UIImage(named: imageName, in: .module, compatibleWith: nil)
+    }
+}
